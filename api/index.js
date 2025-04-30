fa4edd13db2734b25d20d238cd3b0bf95aea907c
MuhammadSesarafliAljagra <muhammad.sesarafli@ui.ac.id>
2025-04-30 14:02:28 +0700

    Vercel adds

--- api/index.js
+++ api/index.js
@@ -19,6 +19,10 @@ app.use("/user", require("../src/routes/userRoute"));
 app.use("/item", require("../src/routes/item.routes"));
 app.use("/transaction", require("../src/routes/transactionRoute"));
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
